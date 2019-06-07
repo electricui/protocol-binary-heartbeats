b4d518553bc8cde5a696d1d06b9ac3bdb896528b
Michael Orenstein <michael@whiteshadows.me>
2019-06-07 14:56:56 +0930

    chore: Bumps

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,15 @@
+/**
+ * Returns the average of a list of numbers
+ */
 export function average(values: Array<number>) {
   const sum = values.reduce((sum, value) => sum + value, 0)
 
   return sum / values.length
 }
 
+/**
+ * Returns the standard deviation of a list of numbers
+ */
 export function standardDeviation(values: Array<number>) {
   const avg = average(values)
 
